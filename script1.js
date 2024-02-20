a7af4759334f80d21f5705030744bdfded2b56ab
KerolisAymen <143899679+KerolisAymen@users.noreply.github.com>
2024-02-20 18:24:00 +0200

    Add files via upload

--- script1.js
+++ script1.js
@@ -20,28 +20,26 @@ function fetchQuestionsFromFile(filename) {
       return [];
     });
 }
-
-// Usage example
-const filename = "data.txt";
-fetchQuestionsFromFile(filename).then((questions) => {
-  console.log(questions[0].question);
-  for (let i = 0; i < questions.length; i++) {
-    makequestion(
-      i + 1,
-      questions[i].question,
-      questions[i].options[0],
-      questions[i].options[1],
-      questions[i].options[2],
-      questions[i].options[3]
-    );
-  }
-});
-
+function createoption(l, ans) {
+  let option = document.createElement("div");
+  option.className = "option";
+  
+  let letter = document.createElement("div");
+  letter.className = "letter";
+  letter.innerText = l;
+  option.appendChild(letter);
+  let answer = document.createElement("p");
+  answer.className = "answer";
+  answer.innerText = ans;
+  option.appendChild(answer);
+  return option;
+}
 function makequestion(qnumber, q, one, two, three, four) {
-  let questions = document.getElementsByClassName("questions")[0];
+ 
+   
   let question = document.createElement("div");
   question.className = "question";
-  questions.appendChild(question);
+  
 
   let questionNumberBox = document.createElement("div");
   questionNumberBox.className = "questionNumberBox";
@@ -65,42 +63,53 @@ function makequestion(qnumber, q, one, two, three, four) {
   options.className = "options";
   div.appendChild(options);
 
-  createoption("أ", one);
-  createoption("ب", two);
-  createoption("ج", three);
-  createoption("د", four);
+  options.appendChild( createoption("أ", one));
+  options.appendChild(createoption("ب", two));
+  options.appendChild(createoption("ج", three));
+  options.appendChild(createoption("د", four));
+  
 
-  // Get all flex options
-  const options2 = document.querySelectorAll(".option");
-
-  // Find the width of the widest sibling
-  let maxWidth = 0;
-  for (let i = options2.length - 1; i >= options2.length - 4; i--) {
-    maxWidth = Math.max(maxWidth, options2[i].offsetWidth);
-  }
-
-  // Apply the width to all flex options
-  for (let i = options2.length - 1; i >= options2.length - 4; i--) {
-    options2[i].style.width = maxWidth + 1 + "px";
-  }
-
-  if (one == undefined) {
-    for (let i = options2.length - 1; i >= options2.length - 4; i--) {
-      options2[i].style.width = "-webkit-fill-available";
-    }
-  }
-
-  function createoption(l, ans) {
-    let option = document.createElement("div");
-    option.className = "option";
-    options.appendChild(option);
-    let letter = document.createElement("div");
-    letter.className = "letter";
-    letter.innerText = l;
-    option.appendChild(letter);
-    let answer = document.createElement("p");
-    answer.className = "answer";
-    answer.innerText = ans;
-    option.appendChild(answer);
-  }
+  return question;
+  
 }
+
+function adjustwidth(selector,one){
+ // Get all flex options
+ const options2 = document.querySelectorAll(selector);
+
+ // Find the width of the widest sibling
+ let maxWidth = 0;
+ for (let i = options2.length - 1; i >= options2.length - 4; i--) {
+   maxWidth = Math.max(maxWidth, options2[i].offsetWidth);
+ }
+
+ // Apply the width to all flex options
+ for (let i = options2.length - 1; i >= options2.length - 4; i--) {
+   options2[i].style.width = maxWidth + 1 + "px";
+ }
+
+ if (one == undefined) {
+   for (let i = options2.length - 1; i >= options2.length - 4; i--) {
+     options2[i].style.width = "-webkit-fill-available";
+   }
+ }
+}
+// Usage example
+const filename = "data.txt";
+fetchQuestionsFromFile(filename).then((questions) => {
+  console.log(questions[0].question);
+  for (let i = 0; i < questions.length; i++) {
+    const question = makequestion(
+      i + 1,
+      questions[i].question,
+      questions[i].options[0],
+      questions[i].options[1],
+      questions[i].options[2],
+      questions[i].options[3]
+    );
+    let questions2 = document.getElementsByClassName("questions")[0];
+    questions2.appendChild(question);
+    adjustwidth(".option",questions[i].options[0]);
+  }
+});
+
